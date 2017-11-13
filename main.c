bbdcd60b6f47d627a1bb8fb1e27e728bc3ee486d
sabdelhay <31925222+sabdelhay@users.noreply.github.com>
2017-11-13 11:34:57 -0500

    Copyright - Author's name correction

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * main.c
- * Copyright (C) 2017 DevC <devc@DevC>
+ * Copyright (C) 2017 DevC S. Abdelhay
  * 
  * TowPoints is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
