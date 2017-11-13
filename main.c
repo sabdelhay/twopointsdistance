c875f0fd6f3ec61ab6dcddf58390c1478d7c6189
sabdelhay <31925222+sabdelhay@users.noreply.github.com>
2017-11-13 11:32:40 -0500

    The final Code

--- main.c
+++ main.c
@@ -1,58 +1,62 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * main.c
- * Copyright (C) 2017 sherif Abdelhay <sabdelhay@gmx.com>
+ * Copyright (C) 2017 DevC <devc@DevC>
  * 
- * TwoPointsDistance is free software: you can redistribute it and/or modify it
+ * TowPoints is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * TwoPointsDistance is distributed in the hope that it will be useful, but
+ * TowPoints is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
+ * 
+ * TwoPoints is a software to measure the distance between two points in 3D space
  * 
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
 
 
-void twopoints(){
+int main(){
+		
+	double x0 = 0;
+	double y0 = 0;
+	double z0 = 0;
+	double x1 = 0;
+	double y1 = 0;
+	double z1 = 0;
 
-    int x1,y1,z1,x2,y2,z2;
-    double *point1[] = {x1,y1,z1};
-    double *point2[] = {x2,y2,z2};
-    double base = 2;
-
-    for(int i = 0; i <= 2; i++){
-        scanf("%lf", &point1[i]);
-        scanf("%lf", &point1[i]);
-
-        for(int j =0; j<=2; j++){
-
-          *point1[j] = double pow(double point1[j] , double base);
-          *point2[j] = double pow(double point2[j] , double base);
-        }
-
+	double result[3];
+	double distance = 0;
+	
+	double point_1[] = {x0,y0,z0};
+	
+	double point_2[] = {x1,y1,z1};
+	int size = 3;
+	
+	printf("Enter point 1 coordinates: ");
+	for(int i = 0; i < size; i++){
+		scanf("%lf", &point_1[i]);
+	}
+	printf("Enter point 2 coordinates: ");
+	for(int j = 0; j < size; j++){
+		scanf("%lf", &point_2[j]);
 	}
 
-	int i = 0;
-	int result[i];
-
-    for ( ; i < 3 ; ++i){
-
-        result[i] = point2[i] - point1[i];
-        printf("%d ", result[i]);
-    }
-}
-
-int main()
-{
-    twopoints();
+	
+	for(int i = 0; i < size; i++){
+		result[i] = sqrt(abs(point_2[i] - point_1[i]));
+		distance += result[i];
+	}
+	printf("Distance is: %.4f\n", distance);
+		
+	return 0;
 }
 
